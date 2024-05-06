3dd9772846b9b13e35b2c5d1d530cc13b39f1632
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-06 18:14:44 +0200

    Added new views to front header

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,16 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <div>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/order">Order</RouterLink>
+        </div>
+        <h1>Boorgir</h1>
+        <div>
+          <!-- Language button -->
+          <RouterLink to="/register">Register</RouterLink>
+          <RouterLink to="/login">Login</RouterLink>
+        </div>
       </nav>
     </div>
   </header>
