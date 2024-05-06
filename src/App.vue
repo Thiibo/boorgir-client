ff61ea0bc7f1bd5c41d8ecd6e185aeb9dac8733a
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-06 19:15:41 +0200

    Styled header

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,53 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <div>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/order">Order</RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/order">Order</RouterLink>
+      <div class="wrapper">
+        <h1>Boorgir</h1>
       </div>
-      <h1>Boorgir</h1>
-      <div>
-        <!-- Language button -->
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </div>
+      <!-- Language button -->
+      <RouterLink to="/register">Register</RouterLink>
+      <RouterLink to="/login" class="button">Login</RouterLink>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  nav {
+    display: flex;
+    position: relative;
+    font-size: 1.2rem;
+    gap: 4rem;
 
+    .wrapper {
+      flex: 1;
+      
+      h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        font-size: 2em;
+        font-weight: bold;
+      }
+    }
+
+    a:not(.button) {
+      &::after {
+        content: '';
+        display: block;
+        height: .2ex;
+        width: 0;
+        background-color: var(--color-text);
+        transition: width .2s ease-in-out;
+      }
+
+      &.router-link-active::after {
+        width: 100%;
+      }
+    }
+  }
 </style>
