fc6992ab1764040b64cbc1c35fbb6fcb5f8ac30f
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-08 16:53:51 +0200

    Make register page work

--- src/modules/api-services/auth.ts
+++ src/modules/api-services/auth.ts
@@ -4,4 +4,8 @@ async function login(email: string, password: string) {
     return API.post('login', { email, password });
 }
 
-export { login };
+async function register(name: string, email: string, password: string, passwordConfirmation: string) {
+    return API.post('register', { name, email, password, password_confirmation: passwordConfirmation });
+}
+
+export { login, register };
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
   import { ref } from 'vue';
+  import { register } from '@/modules/api-services/auth';
   import SideForm from '../components/SideForm.vue';
-  import ValidatedInput from '@/components/ValidatedInput.vue';
+  import ValidationErrors from '@/components/ValidationErrors.vue';
+  import type { ValidationError } from '@/modules/core/validation-error';
 
+  const validationErrors = ref<StringArrayKeyValueObject>();
+
+  const name = ref("");
   const email = ref("");
   const password = ref("");
-  const password_confirmation = ref("");
+  const passwordConfirmation = ref("");
   function submit() {
-
+    register(name.value, email.value, password.value, passwordConfirmation.value)
+      .then(res => console.log(res))
+      .catch((error: ValidationError) => validationErrors.value = error.errors);
   }
 </script>
 
@@ -15,9 +22,22 @@
   <main>
     <SideForm title="Register">
       <form @submit.prevent="submit">
-        <ValidatedInput id="email" label="Email address:" required type="text" v-model="email" />
-        <ValidatedInput id="password" label="Password:" required type="password" v-model="password" />
-        <ValidatedInput id="password_confirmation" label="Repeat password:" required type="password" v-model="password_confirmation" />
+        <label for="name">Name:</label>
+        <input id="name" name="name" type="text" required v-model="name">
+        <ValidationErrors :errors="validationErrors?.name" />
+
+        <label for="email">Email address:</label>
+        <input id="email" name="email" type="email" required v-model="email">
+        <ValidationErrors :errors="validationErrors?.email" />
+
+        <label for="password">Password:</label>
+        <input id="password" name="password" type="password" required v-model="password">
+        <ValidationErrors :errors="validationErrors?.password" />
+
+        <label for="password-confirm">Repeat Password:</label>
+        <input id="password-confirm" name="password-confirm" type="password" required v-model="passwordConfirmation">
+        <ValidationErrors :errors="validationErrors?.password" />
+
         <button>Register</button>
       </form>
     </SideForm>
