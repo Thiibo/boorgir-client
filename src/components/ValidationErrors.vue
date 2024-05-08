90d612f83ff1bca727d11405516c1af75fe1786a
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-08 16:42:34 +0200

    Make login page work

---
+++ src/components/ValidationErrors.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    defineProps<{
+        errors?: string[]
+    }>();
+</script>
+
+<template>
+    <ul class="validation-errors" v-if="errors">
+        <li v-for="error in errors">{{ error }}</li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+    .validation-errors {
+        margin-top: .4rem;
+        padding-left: 2ex;
+        list-style: symbols('-');
+        color: rgb(253, 67, 67);
+    }
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
   import { ref } from 'vue';
-  import { login } from '@/modules/auth/authservice';
+  import { login } from '@/modules/api-services/auth';
   import SideForm from '@/components/SideForm.vue';
-  import ValidatedInput from '@/components/ValidatedInput.vue';
+  import ValidationErrors from '@/components/ValidationErrors.vue';
+  import type { ValidationError } from '@/modules/core/validation-error';
+
+  const validationErrors = ref<StringArrayKeyValueObject>();
 
   const email = ref("");
   const password = ref("");
   function submit() {
-    login(email.value, password.value);
+    login(email.value, password.value)
+      .then(res => console.log(res))
+      .catch((error: ValidationError) => validationErrors.value = error.errors);
   }
 </script>
 
@@ -15,9 +20,16 @@
   <main>
     <SideForm title="Log in">
       <form @submit.prevent="submit">
-        <ValidatedInput id="email" label="Email address:" type="text" required v-model="email" />
-        <ValidatedInput id="password" label="Password:" type="password" required v-model="password" />
+        <label for="email">Email address:</label>
+        <input id="email" name="email" type="email" required v-model="email">
+        <ValidationErrors :errors="validationErrors?.email" />
+
+        <label for="password">Password:</label>
+        <input id="password" name="password" type="password" required v-model="password">
+        <ValidationErrors :errors="validationErrors?.password" />
+
         <button>Log in</button>
+        <ValidationErrors :errors="validationErrors?.general" />
       </form>
     </SideForm>
   </main>
