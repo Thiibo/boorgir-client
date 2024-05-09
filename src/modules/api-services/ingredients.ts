1e670fdc5eaa4a594675f95d15036846c99e4995
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-09 19:35:47 +0200

    Add more endpoints to services

--- src/modules/api-services/ingredients.ts
+++ src/modules/api-services/ingredients.ts
@@ -4,4 +4,8 @@ async function getAll(perPage: number, page: number) {
     return API.get('ingredients', { per_page: perPage.toString(), page: page.toString() }) as Promise<PaginatedApiResult>;
 }
 
-export { getAll };
+async function getAllAdmin(perPage: number, page: number) {
+    return API.get('admin/ingredients', { per_page: perPage.toString(), page: page.toString() }) as Promise<PaginatedApiResult>;
+}
+
+export { getAll, getAllAdmin };
