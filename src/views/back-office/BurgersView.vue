22ae8b95d93f21f20f9a9ae8b4ef7a34e860cfbd
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-09 19:36:10 +0200

    Create basic BurgersView and IngredientsView

--- src/views/back-office/BurgersView.vue
+++ src/views/back-office/BurgersView.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-    
+    import ItemSelection from '@/components/item-selection/ItemSelection.vue';
+    import { getAllAdmin as getAllBurgersAdmin } from '@/modules/api-services/burgers';
 </script>
 
 <template>
-    <div></div>
+    <main>
+        <ItemSelection :item-getter="getAllBurgersAdmin" />
+    </main>
 </template>
+
+<style lang="scss" scoped>
+    main {
+        flex: 1;
+        padding: 3rem;
+    }
+</style>
--- src/views/back-office/IngredientsView.vue
+++ src/views/back-office/IngredientsView.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-    
+    import ItemSelection from '@/components/item-selection/ItemSelection.vue';
+    import { getAllAdmin as getAllIngredientsAdmin } from '@/modules/api-services/ingredients';
 </script>
 
 <template>
-    <div></div>
+    <main>
+        <ItemSelection :item-getter="getAllIngredientsAdmin" />
+    </main>
 </template>
+
+<style lang="scss" scoped>
+    main {
+        flex: 1;
+        padding: 3rem;
+    }
+</style>
