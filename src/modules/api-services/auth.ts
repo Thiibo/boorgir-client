cefd2a046db5d67ea9ac38f3fe44c2b99fd5f78f
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-15 17:54:37 +0200

    Add caching of user profile

--- src/modules/api-services/auth.ts
+++ src/modules/api-services/auth.ts
@@ -1,7 +1,12 @@
+import { ref } from 'vue';
 import API from '../core/api-methods';
 
+const profileData = ref<ProfileApiResult | null>(null);
+
 async function login(email: string, password: string) {
-    return API.post('login', { email, password }) as Promise<ProfileApiResult>;
+    const profile = await API.post('login', { email, password }) as ProfileApiResult;
+    profileData.value = profile;
+    return profile;
 }
 
 async function register(name: string, email: string, password: string, passwordConfirmation: string) {
@@ -9,11 +14,19 @@ async function register(name: string, email: string, password: string, passwordC
 }
 
 async function profile() {
-    return API.get('profile') as Promise<ProfileApiResult>;
+    if (profileData.value) {
+        return profileData.value;
+    } else {
+        const profile = await API.get('profile') as ProfileApiResult;
+        profileData.value = profile;
+        return profile;
+    }
 }
 
 async function logout() {
-    return API.post('logout');
+    const res = await API.post('logout');
+    profileData.value = null;
+    return res;
 }
 
 export { login, register, profile, logout };
