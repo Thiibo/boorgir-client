fa23d8a82b7895632c1ff4c7845759de5563caf6
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-17 15:34:19 +0200

    Add profile info to site header

--- src/components/front/SiteHeader.vue
+++ src/components/front/SiteHeader.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
   import { translate } from '@/modules/core/localization';
   import LocaleButton from '@/components/LocaleButton.vue';
-  import { computed } from 'vue';
+  import { computed, onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue';
   import { useRoute } from 'vue-router';
+  import { isLoggedIn, logout, profileData } from '@/modules/api-services/auth';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { faArrowRight, faArrowRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
+  
   const route = useRoute();
   const routeName = computed(() => route.name);
+
+  const profileElement = ref();
+  const profileDialogOpen = ref(false);
+
+  function clickedSomewhere(e: MouseEvent) {
+    if (e.target !== profileElement.value && !profileElement.value.contains(e.target)) {
+      profileDialogOpen.value = false;
+    }
+  }
+  onMounted(() => document.addEventListener('click', clickedSomewhere));
+  onBeforeUnmount(() => document.removeEventListener('click', clickedSomewhere));
 </script>
 
 <template>
@@ -16,8 +31,26 @@
         <h1 :class="{ enlarge: routeName === 'home' }">Boorgir</h1>
       </div>
       <LocaleButton />
-      <RouterLink to="/register">{{ translate('front.page.register') }}</RouterLink>
-      <RouterLink to="/login" class="button">{{ translate('front.page.login') }}</RouterLink>
+      <RouterLink to="/register" v-if="!isLoggedIn">{{ translate('front.page.register') }}</RouterLink>
+      <RouterLink to="/login" class="button" v-if="!isLoggedIn">{{ translate('front.page.login') }}</RouterLink>
+
+      <div class="profile" ref="profileElement" v-else>
+        <button class="icon-button" @click="profileDialogOpen = !profileDialogOpen">
+          <FontAwesomeIcon :icon="faUser" />
+        </button>
+        <div class="popup" v-show="profileDialogOpen">
+          <span>{{ profileData?.name }}</span>
+          <span class="email">{{ profileData?.email }}</span>
+          <RouterLink to="/admin/ingredients" v-if="profileData?.is_admin">
+            {{ translate("front.page.admin") }}
+            <FontAwesomeIcon :icon="faArrowRight" />
+          </RouterLink>
+          <button class="logout" @click="logout">
+            <FontAwesomeIcon :icon="faArrowRightFromBracket" />
+            {{ translate("general.action.profile.logout") }}
+          </button>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
@@ -66,5 +99,69 @@ nav {
     }
   }
 }
-</style>./LocaleButton.vue
-../LocaleButton.vue
+
+.profile {
+  position: relative;
+
+  > button {
+    padding: .5rem;
+  }
+
+  .popup {
+    position: absolute;
+    top: 3.2rem;
+    right: -.2rem;
+    padding: 1rem;
+    border-radius: 1rem;
+    background-color: var(--color-background-mute);
+    box-shadow: 0 0 2rem 0 rgba(0, 0, 0, .6);
+    
+    &::before {
+      $size: .6rem;
+      content: '';
+      position: absolute;
+      top: -$size * .5;
+      display: block;
+      right: 1rem;
+      width: $size;
+      height: $size;
+      background-color: var(--color-background-mute);
+      transform: rotate(45deg);
+    }
+
+    span {
+      display: block;
+
+      &.email {
+        font-size: .9rem;
+        color: gray;
+      }
+    }
+
+    a {
+      display: block;
+      font-size: 1rem;
+      margin-top: 1rem;
+      white-space: nowrap;
+      width: min-content;
+
+      svg {
+        display: inline-block;
+        margin-left: .5rem;
+      }
+    }
+
+    button {
+      display: block;
+      margin-top: 1rem;
+      width: 100%;
+      white-space: nowrap;
+      z-index: 1;
+
+      svg {
+        margin-right: .3rem;
+      }
+    }
+  }
+}
+</style>
