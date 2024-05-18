9c26511fc93da68638c25fe056ca42cd7fb802b0
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-18 19:48:59 +0200

    Added ingredient overview to BurgerIngredientInput

--- src/components/back-office/BurgerIngredientInput.vue
+++ src/components/back-office/BurgerIngredientInput.vue
@@ -1,36 +1,67 @@
 <script setup lang="ts">
     import { translate } from '@/modules/core/localization';
     import ItemSelectionTable from '../item-selection/ItemSelectionTable.vue';
-    import { ItemService, type IngredientData, type ItemData } from '@/modules/api-services/items';
+    import { ItemService, type IngredientData, type ItemData, getItemTranslatedProperties } from '@/modules/api-services/items';
     import { onMounted, ref, watch } from 'vue';
 
     defineProps<{
-        ingredients?: IngredientData[]
+        ingredients: IngredientData[]
     }>();
 
-    const ingredientsService = new ItemService('ingredients', false);
+    const currentIngredientsService = new ItemService('ingredients', false, {
+        'general.itemselection.column.ingredients.name': item => getItemTranslatedProperties(item)?.name
+    });
+    const addIngredientsService = new ItemService('ingredients', false);
     const data = ref<ItemData[]>();
     const searchQuery = ref('');
 
     async function refreshData() {
-        const requestData = await ingredientsService.getItems(10, 1, searchQuery.value);
+        const requestData = await addIngredientsService.getItems(10, 1, searchQuery.value);
         data.value = requestData.data;
     }
     onMounted(refreshData);
     watch(searchQuery, refreshData);
 
-    defineEmits([
+    const emit = defineEmits([
         "update"
     ]);
 </script>
 
 <template>
     <div v-if="data">
-        <label for="burger-ingredients">{{ translate('general.itemselection.column.burgers.ingredients') }}</label>
-        <ItemSelectionTable
-            :data="data"
-            :item-service="ingredientsService"
-            :action-name="translate('backoffice.itemselection.action.delete')"
-        />
+        <p>{{ translate('general.itemselection.column.burgers.ingredients') }}</p>
+        <div class="content">
+            <ItemSelectionTable
+                class="current-ingredients-table"
+                :data="ingredients"
+                :item-service="currentIngredientsService"
+                :column-names-to-hide="['description']"
+                v-if="ingredients.length > 0"
+            />
+            <button type="button">{{ translate('backoffice.itemselection.action.editingredients') }}</button>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .content {
+        display: inline-block;
+
+        .current-ingredients-table {
+            display: inline-block;
+            width: unset;
+            border: .1rem solid gray;
+        }
+
+        button {
+            vertical-align: top;
+            margin-left: 2rem;
+        }
+    }
+
+    p {
+        display: inline-block;
+        width: 8rem;
+        vertical-align: top;
+    }
+</style>
